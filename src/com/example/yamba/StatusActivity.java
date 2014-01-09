56c78fe6595d2990c41bbb7ea24f46fb2f8350c2
Mohit Jain <jain.mohit27@gmail.com>
2014-01-10 01:55:45 +0530

    twitter post not working

--- src/com/example/yamba/StatusActivity.java
+++ src/com/example/yamba/StatusActivity.java
@@ -1,15 +1,29 @@
 package com.example.yamba;
 
-import android.os.Bundle;
+import winterwell.jtwitter.Twitter;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener {
+	static final String TAG = "Status Activity";
+	EditText editStatus;
+	Button updateButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity);
+		Log.d(TAG, "On Create with Bundle" + savedInstanceState);
+		setContentView(R.layout.status);
+		updateButton = (Button) findViewById(R.id.button_update);
+	    updateButton.setOnClickListener(this);
+
+		editStatus = (EditText) findViewById(R.id.edit_status);
 	}
 
 	@Override
@@ -18,5 +32,25 @@ public class StatusActivity extends Activity {
 		getMenuInflater().inflate(R.menu.status, menu);
 		return true;
 	}
+
+	public void onClick(View view){
+		final String statusText = editStatus.getText().toString();
+		new Thread() {
+			public void run() {
+				try {
+					Twitter twitter = new Twitter("student", "password");
+					twitter.setAPIRootUrl("http://yamba.markana.com/api");
+					twitter.setStatus(statusText);
+				} catch (Exception e) {
+					Log.e(TAG, "Died:", e);
+					e.printStackTrace();
+					// TODO: handle exception
+				}
+			}
+		}.start();
+		Log.d(TAG, "onClick" + statusText);
+		
+	}
+
 
 }
