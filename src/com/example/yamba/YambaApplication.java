4ecd6016eab2f60d5ac8565f0dce95c1f66031d2
Mohit Jain <jain.mohit27@gmail.com>
2014-01-13 23:22:05 +0530

    reading from prefs.. but issue here is changing preference will not change in the call back

--- src/com/example/yamba/YambaApplication.java
+++ src/com/example/yamba/YambaApplication.java
@@ -2,17 +2,28 @@ package com.example.yamba;
 
 import winterwell.jtwitter.Twitter;
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class YambaApplication extends Application {
 	static final String TAG = "YambaApplication";
 	Twitter twitter;
+	SharedPreferences prefs;
+	
 	@Override
 	public void onCreate() {
 		// TODO Auto-generated method stub
 		super.onCreate();
-		twitter = new Twitter("student", "password");
-		twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+		
+		prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		String username = prefs.getString("username", "");
+		String password = prefs.getString("password", "");
+		String server = prefs.getString("server", "");
+
+		twitter = new Twitter(username, password);
+		twitter.setAPIRootUrl(server);
+		
 		Log.d(TAG, "onCreate"); 
 	}
 
