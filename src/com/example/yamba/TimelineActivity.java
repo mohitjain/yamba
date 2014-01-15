a299ebaeeed9e843ceb5b4eda727e7c0ca4172de
Mohit Jain <jain.mohit27@gmail.com>
2014-01-16 01:26:28 +0530

    time is now displayed as 10 mintues ago format..

--- src/com/example/yamba/TimelineActivity.java
+++ src/com/example/yamba/TimelineActivity.java
@@ -3,8 +3,12 @@ package com.example.yamba;
 import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v4.widget.SimpleCursorAdapter;
+import android.text.format.DateUtils;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.SimpleCursorAdapter.ViewBinder;
+import android.widget.TextView;
 
 public class TimelineActivity extends Activity {
 	static final String[] FROM  = {StatusData.C_USER, StatusData.C_TEXT, StatusData.C_CREATED_AT };
@@ -27,12 +31,25 @@ public class TimelineActivity extends Activity {
 //			textOut.append(String.format("\n%s %s", user_name, status_text));
 //		}
 		
-		adapter = new SimpleCursorAdapter(this, R.layout.row, cursor, FROM, TO);  
+		adapter = new SimpleCursorAdapter(this, R.layout.row, cursor, FROM, TO); 
+		adapter.setViewBinder(VIEW_BINDER);
 		list.setAdapter(adapter);
 		
 	}
 	
+	static final ViewBinder VIEW_BINDER = new ViewBinder(){
 
+		@Override
+		public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+			if(view.getId() != R.id.text_created_at) return false;
+			
+			long time = cursor.getLong(cursor.getColumnIndex(StatusData.C_CREATED_AT));
+			CharSequence relative_time = DateUtils.getRelativeTimeSpanString(time);
+			((TextView) view).setText(relative_time); 
+			return true;
+		}
+		
+	}; 
 
 	  
 }
